57bad5acc8ebea1c1ff3b1a7769a00424c99241a
balaji2953 <balaji290503@gmail.com>
2022-09-27 19:51:31 +0530

    prog1

---
+++ Basic programs/name-dob-ph.c
@@ -0,0 +1,16 @@
+//Write a C program to print your name, date of birth. and mobile number.
+/*Expected Output:
+Name   : Alexandra Abramov  
+DOB    : July 14, 1975  
+Mobile : 99-9999999999*/
+
+#include<stdio.h>
+int main()
+{
+    printf("Name : BALAJI\n");
+    printf("DOB : Jan 4,2003\n");
+    printf("Mobile : 99-9999999999");
+    return 0;
+
+    
+}
