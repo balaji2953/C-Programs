2d3b3474bc221fdc248629dcae01ca83e1fadeba
Balaji P <109929136+balaji2953@users.noreply.github.com>
2022-09-28 07:06:51 +0530

    per&area_of_rect

---
+++ Basic Programms/per_and_area_of_rectangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int height=7,width=5;
+	float per,area;
+	
+	per=2*(height+width);
+	area=height*width;
+	
+	printf("perameter of the rectangle=%f",per);
+	printf("Area of the rectangle=%f",area);
+	
+	return 0;
+
+	
+}
