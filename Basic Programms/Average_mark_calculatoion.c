c815b2c05e70f47b23cdfb68ba856c985890609a
Balaji P <109929136+balaji2953@users.noreply.github.com>
2023-01-29 13:15:15 +0530

    Average_calculation

--- Basic Programms/Average_mark_calculatoion.c
+++ Basic Programms/Average_mark_calculatoion.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
 
+int main()
+{
+ int M1,M2,M3,M4,M5,Total_mark;
+ float Average_mark;
+
+ printf("Enter Mark1=");
+ scanf("%d",&M1);
+ printf("Enter Mark2=");
+ scanf("%d",&M2);
+ printf("Enter Mark3=");
+ scanf("%d",&M3);
+ printf("Enter Mark4=");
+ scanf("%d",&M4);
+ printf("Enter Mark5=");
+ scanf("%d",&M5);
+
+ Total_mark=M1+M2+M3+M4+M5;
+ Average_mark=Total_mark/5;
+
+ printf("\nTotal Mark=%d",Total_mark);
+ printf("\nAverage Mark=%0.2f",Average_mark);
+ return 0;
+}
